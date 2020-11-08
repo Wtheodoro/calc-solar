99ff06bf3a6ffde29803f442a49c9c2c35af777e
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-11-08 12:17:00 -0300

    up.

---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Calculator () {
+
+
+    return (
+            <>
+                <h1>Hellor world from calculator</h1>
+                <div className="consumption">
+                    <label>Consumo médio anual:</label><br></br>
+                    <input type="number" className="input" placeholder="Ex: 250"></input>
+                </div>
+                <div>
+                    <h3></h3>
+                </div>
+            </>
+    )
+}
+
+export default Calculator
