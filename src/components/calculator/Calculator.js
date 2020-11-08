9adbf6020908b8472cc550fbbc446794beec8079
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-11-08 14:48:15 -0300

    up.

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Calculator () {
 
+    const [result, setResult] = useState(0)
+    const [consumption, setConsumption] = useState(0)
+    const [city, setCity] = useState("")
+
+    const sumValues = (consumption, city) => {
+        return consumption + city
+    }
 
     return (
             <>
                 <h1>Hellor world from calculator</h1>
                 <div className="consumption">
                     <label>Consumo médio anual:</label><br></br>
-                    <input type="number" className="input" placeholder="Ex: 250"></input>
+                    <input type="number" className="input" placeholder="Ex: 250"
+                    onChangeText={(valueConsu) => valueConsu.length ? setConsumption(parseInt(valueConsu)): setConsumption(0)}
+                    value={consumption.toString(10)}>
+                    </input>
                 </div>
-                <div>
-                    <h3></h3>
+                <div className="select">
+                    <label>Cidade:</label>
+                    <select name="city">
+                        <option value="default">--selecione--</option>
+                        <option value="campinas">Campinas-SP</option>
+                        <option value="teresina">Teresina-PI</option>
+                        <option value="belem">Belem-PA</option>
+                        <option value="cubatão">Cubatão-SP</option>
+                        <option value="curitiba">Curitiba-PR</option>
+                    </select>
+                </div>
+                <div className="result">
+                    <label name="result"></label>
+                    <span>result came here: {result}</span>
                 </div>
             </>
     )
