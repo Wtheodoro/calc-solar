93bb71e410020f2261686e34aff252946a9ea147
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-11-08 21:51:31 -0300

    up.

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -2,32 +2,20 @@ import React, { useState } from 'react'
 
 function Calculator () {
 
-    const [result, setResult] = useState(0)
-    const [consumption, setConsumption] = useState("")
+    const [consumption, setConsumption] = useState(0)
     const [city, setCity] = useState("Toxy City")
+    const [result, setResult] = useState(0)
 
-    // const sumValues = (consumption, city) => {
-    //     return (
-    //         result = consumption + city
-    //         )
-    // }
     function getValue(event) {
-        const { name, value } = event.target
+        setConsumption(event.value)
         
-        console.log(event.target)
+        setConsumption(consumption + 1)
     }
  
     return (
-            <>
-                <h1>Hellor world from calculator</h1>
-                <div className="consumption">
-                    <label>Consumo médio anual:</label><br></br>
-                    {/* <input type="number" className="input" placeholder="Ex: 250"
-                    onChangeText={(valueConsu) => valueConsu.length ? setConsumption(parseInt(valueConsu)): setConsumption(0)}
-                    value={consumption.toString(10)}> */}
-                    <input type="number" name="consumption" className="input" placeholder="em KWh" onChange={getValue}>
-                    </input>
-                </div>
+            <> 
+                <h1>Hellor world from calculator</h1> 
+                
                 <div className="select">
                     <label>Cidade:</label>
                     <select name="city">
@@ -47,9 +35,17 @@ function Calculator () {
                         <option value="100">Trifásico</option>
                     </select>
                 </div>
+                <div className="consumption">
+                    <label>Consumo médio anual:</label><br></br>
+                    {/* <input type="number" className="input" placeholder="Ex: 250"
+                    onChangeText={(valueConsu) => valueConsu.length ? setConsumption(parseInt(valueConsu)): setConsumption(0)}
+                    value={consumption.toString(10)}> */}
+                    <input type="number" name="consumption" className="input" placeholder="em KWh" onChange={getValue}>
+                    </input>
+                </div>
                 <div className="result">
                     <label name="result"></label>
-                    <span>result came here: {consumption}</span>
+                    <span>result came here: {consumption} -- {city} -- {result}</span>
                 </div>
             </>
     )
