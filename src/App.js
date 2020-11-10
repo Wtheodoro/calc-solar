d08df9517109db0fcb3e66bdd9191ca7c1509e2e
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-11-09 22:19:38 -0300

    up.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from './components/Header/Header'
 
 function App() {
   return (
-    <>
+    <div className="page">
       <Header />
       <Calculator />
-    </>
+    </div>
   );
 }
 
